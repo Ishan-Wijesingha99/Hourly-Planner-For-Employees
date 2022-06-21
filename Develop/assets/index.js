28999d7c38dbb879652523c4836d2b178783c5fb
Ishan <you@example.com>
2022-06-22 00:21:21 +1000

    finished adding date and other grid rows

--- Develop/assets/index.js
+++ Develop/assets/index.js
@@ -1,3 +1,20 @@
+// defining HTML elements
+const currentTimeElement = document.querySelector('#currentDay');
 
 
-console.log("HELLO!")
+// defining dates
+const locale = navigator.language;
+const currentTime = new Date();
+const currentDay = currentTime.toLocaleString(locale, {weekday: 'long'});
+const currentDate = currentTime.getDate();
+const currentMonth = currentTime.toLocaleString(locale, {month: 'long'})
+
+
+
+currentTimeElement.textContent = `${currentDay} ${currentDate} ${currentMonth} `
+
+
+
+
+
+
